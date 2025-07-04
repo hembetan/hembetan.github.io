45148a62d4f19769c13ab9183b50e179eb8ba225
Hector <hembetan@hotmail.com>
2025-07-03 20:21:40 -0600

    Se agregaron imagen en título, barra de navegación y navegación suave

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const logos = document.getElementById('logos');
-    const links = document.getElementById('links');
-    const calendar = document.getElementById('calendar');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const logos = document.getElementById('logos');
+//     const links = document.getElementById('links');
+//     const calendar = document.getElementById('calendar');
 
-    logos.addEventListener('click', () => {
-        if (links.style.display != 'none') {
-            links.style.display = 'none';
-            calendar.style.display = 'flex';
-        } else {
-            links.style.display = 'flex';
-            calendar.style.display = 'none';
-        }
-    });
-});
+//     logos.addEventListener('click', () => {
+//         if (links.style.display != 'none') {
+//             links.style.display = 'none';
+//             calendar.style.display = 'flex';
+//         } else {
+//             links.style.display = 'flex';
+//             calendar.style.display = 'none';
+//         }
+//     });
+// });
